325eeee67509733ab1f09dfe964798f897bec03a
jsemeraro <julia.semeraro@outlook.com>
2018-02-05 03:51:45 -0500

    added tests

--- lib/calc.ex
+++ lib/calc.ex
@@ -10,10 +10,9 @@ defmodule Calc do
     first_elem = List.first(parsed_list)
     if determine_num?(first_elem) or first_elem == "(" do
       tree = build_tree(parsed_list)
-      IO.puts("tree: " <> inspect tree)
-      IO.puts(solving(tree))
+      solving(tree)
     else
-      IO.puts("Error: invalid input")
+      raise("Error: invalid input")
     end
   end
 
@@ -71,8 +70,6 @@ defmodule Calc do
           find_close(tail, ct, pos+1)
       end
     else
-      IO.puts("find_close first " <> inspect first)
-      IO.puts("find_close tail " <> inspect tail)
       IO.puts("Error: invalid parens")
       nil
     end
@@ -94,52 +91,46 @@ defmodule Calc do
       "-" -> :-
       "*" -> :*
       "/" -> :/
+      _ ->
+        raise("Error: invalid operator")
     end
   end
 
 
   def build_tree(equ_list) do
     if !is_nil(equ_list) && !Enum.empty?(equ_list) do
-      IO.puts("FIRST of equ_list " <> inspect equ_list)
       equ_list = trim_parens(equ_list)
-      IO.puts("trimmed list " <> inspect equ_list)
       [first | tail] = equ_list
       
       cond do
         determine_open(first) ->
           # find position of closing paren
           close = find_close(tail, 1, 0)
-          IO.puts("build_tree DO close pos " <> inspect close)
-          
-          # this is the tail which should be everything but the first paren
-          IO.puts("build_tree DO tail: " <> inspect tail)
           
           # grabs everything in the parens and evaluates to the left
           left = Enum.slice(tail, 0..close-1)
-          IO.puts("build_tree DO left " <> inspect left)
           
           # should grab the second operand
           right_list = Enum.slice(tail, close+1..-1)
 
           op = find_op(right_list)
-          IO.puts("op " <> inspect Enum.at(right_list, op))
           
           # grabs the rest of the equation and throws it in the right
           right = Enum.slice(right_list, op+1..-1)
-          IO.puts("build_tree DO right " <> inspect right)
           
           %Tree{op: Enum.at(right_list, op) |> parse_ops, left: build_tree(left), right: build_tree(right)}
 
 
         !is_nil(find_op(equ_list)) ->
           left = Enum.slice(equ_list, 0..find_op(equ_list)-1)
-          IO.puts("NIL left " <> inspect left)
           right = Enum.slice(equ_list, find_op(equ_list)+1..-1)
-          IO.puts("NIL right " <> inspect right)
           %Tree{op: Enum.at(equ_list, find_op(equ_list)) |> parse_ops, left: build_tree(left), right: build_tree(right)}
 
         determine_num?(first) ->
           String.to_integer(first)
+
+        true ->
+          raise("Error: unknown symbol")
       end
     else
       nil
@@ -152,7 +143,8 @@ defmodule Calc do
 
   def main() do
     # found gets here: https://hexdocs.pm/elixir/IO.html#gets/2
-    IO.gets("Enter a math equation: ") |> eval
+    ans = IO.gets("Enter a math equation: ") |> eval
+    IO.puts(ans)
     main()
   end
 end
--- test/calc_test.exs
+++ test/calc_test.exs
@@ -2,7 +2,34 @@ defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
 
-  test "greets the world" do
-    assert Calc.hello() == :world
+  test "simple addition" do
+    assert Calc.eval("2 + 3") == 5
+    assert Calc.eval("1 + ( 3 + 4 )") == 8
+    assert Calc.eval("( 3 + 4 ) + 1") == 8
+  end
+
+  test "simple subtraction" do
+    assert Calc.eval("2 - 3") == -1
+    assert Calc.eval("1 - ( 3 - 4 )") == 2
+    assert Calc.eval("( 4 - 3 ) - 1") == 0
+  end
+
+  test "simple multiplication" do
+    assert Calc.eval("2 * 3") == 6
+    assert Calc.eval("3 * ( 4 * 1 )") == 12
+    assert Calc.eval("5 * 5") == 25
+  end
+
+  test "simple division" do
+    assert Calc.eval("4 / 2") == 2
+    assert Calc.eval("16 / ( 8 / 2 )") == 4
+    assert Calc.eval("( 12 / 3 ) / 2") == 2
+  end
+
+  test "other tests" do
+    assert Calc.eval("( 2 + 2 ) * 4") == 16
+    assert Calc.eval("( 5 + 5 ) / 2") == 5
+    assert Calc.eval("( 3 + 3 ) - 6") == 0
+    assert Calc.eval("( 4 + ( 6 * 2 ) + 5 ) * 2 ") == 42
   end
 end
